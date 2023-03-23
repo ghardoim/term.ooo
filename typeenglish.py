6f18b85dad47c84f8a39a575dce56265aa56f54e
Gabriel Hardoim <37919492+ghardoim@users.noreply.github.com>
2023-03-23 18:27:09 -0300

    Update typeenglish.py

--- typeenglish.py
+++ typeenglish.py
@@ -14,25 +14,20 @@ class English(TypeWord):
         self._browser.find_element(By.CSS_SELECTOR, "[aria-label='Close']").click()
         self._actions = ActionChains(self._browser)
 
-        self._flags, new_words = {"--has": "", "--not-has": "", "--is-in": "", "--not-is-in": ""}, []
+        flags = {"--has": "", "--not-has": "", "--is-in": "", "--not-is-in": ""}
         board = self._browser.find_element(By.XPATH, "//div[contains(@class,'Board')]")
-
         for r, row in enumerate(board.find_elements(By.XPATH, "//div[contains(@class,'Row')]")):
             if self._is_all(row, "empty"):
                 while True:
-
-                    self._guess(r, row, new_words, "en-us")
+                    self._guess(r, row, flags, "en-us")
                     if self._is_all(row, "tbd"):
                         [ self._actions.send_keys_to_element(cell, Keys.BACKSPACE).perform() for cell in self._get_all(row, "tbd") ]
                     else: break
-
             sleep(3)
-            self._analyze_guess(row, "correct", "absent", "present")
-            if not self._is_all(row, "correct"): new_words = self._get_new_words("en-us")
-            else: break
+            flags = self._analyze_guess(row, flags, "correct", "absent", "present")
+            if self._is_all(row, "correct"): break
 
     def _get_all(self, row:WebElement, kind:str) -> list: return row.find_elements(By.CSS_SELECTOR, "[data-state='%s']" % (kind))
-    def _get_guesses(self, row:WebElement, kind:str) -> str: return "".join([l.text for l in self._get_all(row, kind)])
 
     def _get_guess_position(self, row:WebElement, kind:str) -> str:
         return "".join([f" {s.text}{_ + 1}" for _, s in enumerate(row.find_elements(By.CSS_SELECTOR,
