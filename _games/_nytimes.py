564f2e11ca7c2d1cc056250188756c804455c321
Gabriel Hardoim <ghardoim@hotmail.com>
2025-04-06 14:37:04 -0300

    new bot

---
+++ _games/_nytimes.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Safari
+from time import sleep
+
+class Puzzle(Safari):
+    def __init__(self, url:str):
+        super().__init__()
+
+        self.get("https://www.nytimes.com/" + url)
+        self.maximize_window()
+        sleep(2)
+
+        for button in ["//button[contains(text(), '{}')]".format(btn) for btn in ["Reject all", "Continue", "Play"]]:
+            self.execute_script("arguments[0].click()", self.find_element(By.XPATH, button))
